7b8f83afe16378e0c494a99be82a465901091714
Pavlo <pavlof66@gmail.com>
2019-03-20 14:03:23 +0400

    [+] Добавил генерацию команды вместе с именем отправителя

--- Program.cs
+++ Program.cs
@@ -155,6 +155,7 @@ namespace DonatePayStat
                 }
 
                 var allValuesByKey = sValue.Split(',');
+                var what = string.IsNullOrEmpty(tran.what) ? "noname" : Translit(tran.what);
 
                 foreach (var item in allValuesByKey)
                 {
@@ -180,7 +181,7 @@ namespace DonatePayStat
                     for (int i = 0; i < comandCount; i++)
                     {
                         string fileName = Path.Combine(_dir, DateTime.Now.ToString("dd_MM_yyyy_HH_mm_ss_ffff") + $"{i}.spawn");
-                        File.WriteAllText($"{fileName}", command, Encoding.ASCII);
+                        File.WriteAllText($"{fileName}", $"{command};{what}", Encoding.ASCII);
                     }
 
                     if (comandCount > 0)
@@ -424,6 +425,28 @@ namespace DonatePayStat
             {
 
             }
+        }
+
+        public static string Translit(string str)
+        {
+            try
+            {
+                string[] lat_up = { "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "Kh", "Ts", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya" };
+                string[] lat_low = { "a", "b", "v", "g", "d", "e", "yo", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "kh", "ts", "ch", "sh", "shch", "\"", "y", "'", "e", "yu", "ya" };
+                string[] rus_up = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
+                string[] rus_low = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я" };
+                for (int i = 0; i <= 32; i++)
+                {
+                    str = str.Replace(rus_up[i], lat_up[i]);
+                    str = str.Replace(rus_low[i], lat_low[i]);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return str;
         }
     }
 
